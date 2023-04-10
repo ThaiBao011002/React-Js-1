51ce2ad53dc09f789876541e1101c424673bbd90
ThaiBao011002 <123188501+ThaiBao011002@users.noreply.github.com>
2023-04-10 12:56:18 +0700

    React Js

--- baitapthuchanhlayout/src/App.js
+++ baitapthuchanhlayout/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './modules/baitaplayoutcompunent/Header';
+import Item from './modules/baitaplayoutcompunent/Item';
+import Banner from './modules/baitaplayoutcompunent/Banner';
+import Footer from './modules/baitaplayoutcompunent/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'> 
+    <Header/>
+    <Banner/>
+    <Item/>
+    <Footer/>
+
+
+    
+  
+
     </div>
   );
 }
